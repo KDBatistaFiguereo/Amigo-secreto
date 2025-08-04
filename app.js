50f930c5dd5cbd8a7b89358e184dff479791fb4f
bibi <kevinfiguereo40@gmail.com>
2025-08-04 10:49:53 -0400

    "funcionalidad: Aregar funcion para agregar amigos"

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var amigos = [];
+
+function agregarAmigo(){
+  let elementoAmigo = document.getElementById('amigo')
+  let amigo = '';
+
+  if (elementoAmigo != null && elementoAmigo.value != "") {
+    amigo = elementoAmigo.value;
+    elementoAmigo.value = '';
+    amigos.push(amigo);
+  } else {
+    alert("Inserte un nombre");
+  }
+  //debug
+  //console.log(amigos);
+}
+
+
+
